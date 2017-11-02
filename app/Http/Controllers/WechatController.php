43f5e4882cbc9fe678a33f10edb158252f3038ed
ZT <zt8479@gmail.com>
2017-11-02 18:21:21 +0800

    新建wechat项目

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Log;
+
+class WechatController extends Controller
+{
+
+    /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            Log::info('wechat message',[$message]);
+            $content =$message->content;
+            if ($content == 1){
+                return'1';
+            }else{
+                return "欢迎关注 overtrue！";
+            }
+
+        });
+
+        Log::info('return response.');
+
+        return $wechat->server->serve();
+    }
+}
