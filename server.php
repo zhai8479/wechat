b9be2b62e1b44697c87a619c8647597beeae328f
ZT <zt8479@gmail.com>
2017-11-02 22:26:11 +0800

    修改部分配置wechat项目

--- server.php
+++ server.php
@@ -1,21 +1,19 @@
 <?php
-
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
-}
-
-require_once __DIR__.'/public/index.php';
+include __DIR__ . '/vendor/autoload.php'; // 引入 composer 入口文件
+use EasyWeChat\Foundation\Application;
+$options = [
+    'debug'  => true,
+    'app_id' => 'wxde2f745ed82213c6',
+    'secret' => '474ed9fa465d49c7935c94c7fbaddf08',
+    'token'  => 'zhai',
+     'aes_key' => '6fGnKb24X5kxilma6YczQAwGjrF5zzP1CiqEp9E8E37',
+    'log' => [
+        'level' => 'debug',
+        'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+    ],
+    //...
+];
+$app = new Application($options);
+$response = $app->server->serve();
+// 将响应输出
+return $app->server->serve();
