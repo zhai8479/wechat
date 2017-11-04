1c4bbe2eaec5c0674ee67253b6913ce163f4acd9
ZT <zt8479@gmail.com>
2017-11-04 13:14:36 +0800

    添加rmccue request

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Core\Http;
 use Log;
 
 class WechatController extends Controller
@@ -29,7 +30,7 @@ class WechatController extends Controller
                     'info' => $content,
                     'userid' => $userid,
                 ];
-                $response = \Request::post('http://www.tuling123.com/openapi/api', [], $data);
+                $response = Request::post('http://www.tuling123.com/openapi/api', [], $data);
                 if ($response->success) {
                     $daan =json_decode($data);
                     Log::info('$response',[$daan]);
